bf733688662d3653fdf78f0f12894ef6213286ab
leo <leo.yuan@moxlink.com>
2025-02-13 10:25:52 +0800

    fix

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/navigation';
+
+const Posts = ({
+    params,
+  }: {
+    params: Promise<{ id: string }>
+  }) => {
+    
+
+    return <div>Posts: {}</div>;
+}
+
+export default Posts;
+
