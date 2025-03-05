24d48dc80167af17981bbca3ec0406741519282f
leo <leo.yuan@moxlink.com>
2025-03-05 17:14:11 +0800

    fix

---
+++ components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Menu } from 'antd';
+const items = [
+  {
+    label: 'Navigation One',
+    key: 'mail',
+  },
+  {
+    label: 'Navigation Two',
+    key: 'app',
+  },
+  {
+    label: 'Navigation Three',
+    key: 'SubMenu',
+  },
+];
+export default function Header() {
+    const [current, setCurrent] = useState('mail');
+    const onClick = (e) => {
+      console.log('click ', e);
+      setCurrent(e.key);
+    };
+    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+}
